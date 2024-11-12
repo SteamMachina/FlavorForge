d6315071a3340a50d53d6d8fc9f996b4e3e72410
SteamMachina <52200491+SteamMachina@users.noreply.github.com>
2024-11-12 13:02:37 +0100

    set up Profile and home pages

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,22 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
- } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App />,
-    children:[],
-  }
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
